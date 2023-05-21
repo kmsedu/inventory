eb01ecd43ed44fc98fd6609469bb35eeb901fa82
kris <kmsedu@tuta.io>
2023-05-21 12:37:17 +1000

    Set up Pug as view engine and serve static files from /public/

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import express from "express";
 import { router } from "../src/router";
 const app = express();
 
+// Set up server
+app.set("views", "./src/views/");
+app.set("view engine", "pug");
+
+app.use(express.static("public"));
+
 app.use("/", router);
 
 app.listen(8000, () => {
