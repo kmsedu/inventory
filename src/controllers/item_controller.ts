bf19b8b817dc4b933b292ba5015c23ed0775ea5b
kris <kmsedu@tuta.io>
2023-05-21 20:19:02 +1000

    Implemented first draft index page and added some styling for page layout

--- src/controllers/item_controller.ts
+++ src/controllers/item_controller.ts
@@ -1,9 +1,29 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 
+const prisma = new PrismaClient();
+
 const ItemController = {
   index: async function(req: Request, res: Response, next: NextFunction) {
-    res.render("index", { title: "Index" });
+    try {
+      const items = await prisma.item.findMany({}).catch((err) => {
+        throw new Error(err);
+      });
+      const categories = await prisma.category.findMany({}).catch((err) => {
+        throw new Error(err);
+      });
+
+      res.render("index", {
+        title: "Home page",
+        items,
+        categories,
+      });
+    } catch (error) {
+      const err = new Error(error as string);
+
+      err.message = "404 Error";
+      next(err);
+    }
   },
   list: async function(req: Request, res: Response, next: NextFunction) {
     res.render("item_list", { title: "Item list" });
