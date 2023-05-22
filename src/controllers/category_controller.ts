1fc32c4962b6983e54f50e94bbaf7c2532cae752
kris <kmsedu@tuta.io>
2023-05-22 22:28:31 +1000

    Implemented category GET views

--- src/controllers/category_controller.ts
+++ src/controllers/category_controller.ts
@@ -1,12 +1,49 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
+import { wrap } from "module";
+
+const prisma = new PrismaClient();
 
 const CategoryController = {
   detail: async function(req: Request, res: Response, next: NextFunction) {
-    res.render("category_detail", { title: "Category Detail" });
+    if (req.params.id === null) {
+      const err = new Error("No ID parameter found in URL");
+      const httpError = { ...err, status: 200 };
+      next(httpError);
+    }
+
+    const category = await prisma.category.findUnique({
+      where: { id: req.params.id },
+      include: { items: true },
+    });
+
+    if (category !== null) {
+      res.render("category_detail", {
+        title: category.name,
+        category,
+      });
+    }
   },
   list: async function(req: Request, res: Response, next: NextFunction) {
-    res.render("category_list", { title: "Category List" });
+    const categories = await prisma.category.findMany();
+
+    if (categories.length !== 0) {
+      const itemCounts = categories.map((category) => {
+        return category.itemIds.length;
+      });
+
+      res.render("category_list", {
+        title: "Category List",
+        categories,
+        itemCounts,
+      });
+      return;
+    }
+
+    const err = new Error("Categories not found");
+    const httpError = { ...err, status: 404 };
+
+    next(httpError);
   },
   create: async function(req: Request, res: Response, next: NextFunction) {
     res.render("category_create", { title: "Category create" });
