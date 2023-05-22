0455bdda088af460e5f1f9370fdbd8e37cfbd082
kris <kmsedu@tuta.io>
2023-05-22 21:37:19 +1000

    Removed StockItem from router

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { ItemController } from "./controllers/item_controller";
 import { CategoryController } from "./controllers/category_controller";
-import { StockItemController } from "./controllers/stock_item_controller";
 
 const router = express.Router();
 
@@ -31,17 +30,5 @@ router.get("/category/:id", CategoryController.detail);
 router.post("/category/create", CategoryController.createPost);
 router.post("/category/:id/update", CategoryController.updatePost);
 router.post("/category/:id/delete", CategoryController.deletePost);
-
-// StockItem GET routing
-router.get("/stockitems", StockItemController.list);
-router.get("/stockitem/create", StockItemController.create);
-router.get("/stockitem/:id/update", StockItemController.update);
-router.get("/stockitem/:id/delete", StockItemController.delete);
-router.get("/stockitem/:id", StockItemController.detail);
-
-// StockItem POST routing
-router.post("/stockitem/create", StockItemController.createPost);
-router.post("/stockitem/:id/update", StockItemController.updatePost);
-router.post("/stockitem/:id/delete", StockItemController.deletePost);
 
 export { router };
